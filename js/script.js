8c0e96ab5f10ceab460224f966850797359262f2
MATHEUS HENRIQUE DE MACEDO <67766992+nrick16@users.noreply.github.com>
2023-12-10 16:10:02 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,23 +1,45 @@
 // Ultimo post do Instagram (IMAGEM)
 $(function () {
 
-    const token = "IGQWRNaWZAPWGJMbXJ5WnAtanlBWUpuR09ZAdWxZAbHhDb2FOSjNJTUxvcDdIbjd0b0NSR1lDcXdvWElxMDNDNTFaZA3JLOWxMdTkzYzNNb2FFTVBrZATZAQVnNNaGpES2FuZAkRtUUZAqbG55ZA216VEU0LUx4M19KZATc3NVEZD"
+    //const token = "IGQWRQaDdkR0NxWlhMQ0ltY1R3cVVOeXlEZA0U3SnlwZAXUycTRlZAENDcW8xbzYxdkY0WDBLdzZAMZA0gwQzY3OE1EdTg4VmE4YV96aVFBbGxOR19ZAeW0za2lKWGtTd2U1YkJiTWp6M0Y2V0J5SURTV25vcnBvaWNGNjAZD"
+    const token = "IGQWROSU9yMG0zWWdrbG8xT2Q0dl9YQmc5NXM4SXh6Vjh1aWhvLVJDNENsMGt2Mmptb29FQURRRVFQUGN2UmVyRmhxZATZAuUVltZA19RWmpOaTBEcXUycXQ5d0JEYTN0YXdfXzJPQTZARYkp2djg1WmRBQm82VkNwOTQZD"
     const url = "https://graph.instagram.com/me/media?access_token=" + token + "&fields=media_url,media_type,caption,permalink"
 
     $.get(url).then(function (response) {
         console.log('return: ', response.data);
-
+        
         // Variável contendo o link da ultima imagem do instagram
-        const last_post = response.data[1];
+        const last_post = response.data[0];
+        const dataJson = response.data;
+        let content = '<div class="row" style="padding-left:5px">';
 
-        const linkDaImagem = response.data[1].media_url;
+        const linkDaImagem = response.data[0].media_url;
         const textPost = last_post.caption !== null ? last_post.caption : '';
        
         // Obtendo a referência da tag IMG pelo ID e atribuindo a variável ao atributo src
         document.getElementById('imagem-insta').src = linkDaImagem;
         document.getElementById('titulo-insta').textContent = textPost;
+
+        for(let p = 0; p < response.data.length; p++){
+                console.log('return do response Data: ', response.data[p]);
+                console.log('return do response media type: ', response.data[p].media_type);
+                let type = response.data[p].media_type;
+                console.log('return do response Data: ',type);
+
+                if(response.data[p].media_type === 'IMAGE')   {
+                    console.log('return do if media URL: ', response.data[p].media_url);
+                    let feed = document.getElementById('insta').src = response.data[p].media_url;
+
+                    content += '<div><img id="insta" alt=""></div>';
+                }
+        }
+        content += '</div>';
+        
     })
+
 });
+
+
 
 window.addEventListener('scroll', function(){
     var menu = document.querySelector('.menu');
